292f602d0bfc0c01a14456bdf609a2a5f4246ecf
Scott Jacobsen <jacobsenscott@yahoo.com>
2015-01-08 23:19:43 -0700

    Give gmail folders sensible names.

--- tag-offlineimap/offlineimap.py
+++ tag-offlineimap/offlineimap.py
@@ -1,6 +1,24 @@
 import os
 import subprocess
 import sys
+import re
+
+def nametrans_remote(folder):
+  return re.sub("^\[Gmail\]/Drafts$", "drafts",
+  re.sub("^\[Gmail\]/Important$", "important",
+  re.sub("^\[Gmail\]/Sent Mail$", "sent",
+  re.sub("^\[Gmail\]/Starred$", "flagged",
+  re.sub("^INBOX$", "inbox", folder)))))
+
+def nametrans_local(folder):
+  return re.sub('^drafts$',    '[Gmail].Drafts',
+  re.sub('^important$', '[Gmail].Important',
+  re.sub('^sent$',      '[Gmail].Sent Mail',
+  re.sub('^flagged$',   '[Gmail].Starred',
+  re.sub('^inbox$',     'INBOX',  folder)))))
+
+def folder_filter(folder):
+  return folder not in ['[Gmail]/All Mail', '[Gmail]/Spam', '[Gmail]/Trash']
 
 def mailpasswd(gpg_file):
   path = "%s/.passwords/%s" % (os.getenv("HOME"), gpg_file)
