d39b2e3e70a141df157bec1052668a5a926523eb
Scott Jacobsen <jacobsenscott@yahoo.com>
2015-01-09 18:09:16 -0700

    Download spam and trash.
    Moving mail to the spam and trash folder is how you mark them as spam
    and trash.

--- tag-offlineimap/offlineimap.py
+++ tag-offlineimap/offlineimap.py
@@ -5,20 +5,22 @@ import re
 
 def nametrans_remote(folder):
   return re.sub("^\[Gmail\]/Drafts$", "drafts",
-  re.sub("^\[Gmail\]/Important$", "important",
+  re.sub("^\[Gmail\]/Trash$", "trash",
+  re.sub("^\[Gmail\]/Spam$", "spam",
   re.sub("^\[Gmail\]/Sent Mail$", "sent",
   re.sub("^\[Gmail\]/Starred$", "flagged",
-  re.sub("^INBOX$", "inbox", folder)))))
+  re.sub("^INBOX$", "inbox", folder))))))
 
 def nametrans_local(folder):
-  return re.sub('^drafts$',    '[Gmail].Drafts',
-  re.sub('^important$', '[Gmail].Important',
-  re.sub('^sent$',      '[Gmail].Sent Mail',
-  re.sub('^flagged$',   '[Gmail].Starred',
-  re.sub('^inbox$',     'INBOX',  folder)))))
+  return re.sub('^drafts$', '[Gmail].Drafts',
+  re.sub('^trash$', '[Gmail].Trash',
+  re.sub('^spam$', '[Gmail].Spam',
+  re.sub('^sent$', '[Gmail].Sent Mail',
+  re.sub('^flagged$', '[Gmail].Starred',
+  re.sub('^inbox$', 'INBOX', folder))))))
 
 def folder_filter(folder):
-  return folder not in ['[Gmail]/All Mail', '[Gmail]/Spam', '[Gmail]/Trash']
+  return folder not in ['[Gmail]/All Mail', '[Gmail]/Important']
 
 def mailpasswd(gpg_file):
   path = "%s/.passwords/%s" % (os.getenv("HOME"), gpg_file)
